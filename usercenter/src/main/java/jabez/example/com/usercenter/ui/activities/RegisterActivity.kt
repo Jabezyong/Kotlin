46b35b753cb3a8a0394a5cc39d66f62b7886e2b7
jabez.yong1215@gmail.com <Kelvin121595>
2018-03-14 09:02:30 +0800

    first init

---
+++ usercenter/src/main/java/jabez/example/com/usercenter/ui/activities/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package jabez.example.com.usercenter.ui.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import jabez.example.com.usercenter.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
